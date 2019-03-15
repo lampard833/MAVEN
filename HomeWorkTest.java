b0011bf00b66d3b9139c34e2ba234a504daac2e7
Oleg <lampard833@mail.ru>
2019-03-15 13:57:46 +0200

    Maven

--- HomeWorkTest.java
+++ HomeWorkTest.java
@@ -2,20 +2,23 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class HomeWorkTest {
 
     @Test
     public void testStudents () {
-        WebDriver stdriver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
-        stdriver.get("http://37.59.228.229:3000/");
-        stdriver.findElement(By.cssSelector("a[class='add-user btn-floating btn-large waves-effect waves-light red']")).click();
-        stdriver.findElement(By.xpath("//input [@id=\"icon_prefix\"]")).clear();
-        stdriver.findElement(By.xpath("//input [@id=\"icon_prefix\"]")).sendKeys("Hellboy");
-        stdriver.findElement(By.xpath("//input [@id=\"icon_telephone\"]")).clear();
-        stdriver.findElement(By.xpath("//input [@id=\"icon_telephone\"]")).sendKeys("123456789");
-        stdriver.findElement(By.xpath("//a [@class='save-btn waves-effect waves-light btn']")).click();
+        driver.get("http://37.59.228.229:3000/");
+        driver.findElement(By.cssSelector("a[class='add-user btn-floating btn-large waves-effect waves-light red']")).click();
+        driver.findElement(By.xpath("//input [@id=\"icon_prefix\"]")).clear();
+        driver.findElement(By.xpath("//input [@id=\"icon_prefix\"]")).sendKeys("Hellboy");
+        driver.findElement(By.xpath("//input [@id=\"icon_telephone\"]")).clear();
+        driver.findElement(By.xpath("//input [@id=\"icon_telephone\"]")).sendKeys("123456789");
+        //Select role = new Select(driver.findElement(By.xpath("//select[@class ='initialized']")));
+        //role.selectByValue("Admin");
+        driver.findElement(By.xpath("//a [@class='save-btn waves-effect waves-light btn']")).click();
 
         }
 }
